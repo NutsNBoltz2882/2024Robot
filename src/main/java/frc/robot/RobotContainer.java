f1daa34f94747c55b95269c3a82af7cfef53e423
Vasska <vasskae@gmail.com>
2024-03-24 12:30:33 -0500

    new auto and chooser

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,11 +17,14 @@ import frc.robot.subsystems.SwerveSubsystem;
 
 import java.io.File;
 
+import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -45,10 +48,17 @@ public class RobotContainer {
   private final Shooter shooter = new Shooter(18, 19);
   private final CommandXboxController driverController = new CommandXboxController(0);
   private final CommandXboxController intakeController = new CommandXboxController(1);
+
+  private final SendableChooser<Command> autoChooser;
   
  
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+
+    autoChooser = AutoBuilder.buildAutoChooser();
+    SmartDashboard.putData("Auto chooser", autoChooser);
+    autoChooser.addOption("Better Drive Forward", new PathPlannerAuto("Drive Forward"));
+    autoChooser.addOption("Simple Drive Forward", new PathPlannerAuto("New Auto"));
     // Configure the trigger bindings
     /*
     drivetrain.setDefaultCommand(drivetrain.arcadeDriveCommand(
@@ -120,6 +130,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return new PathPlannerAuto("New Auto");
+    return autoChooser.getSelected();
   }
 }
