c601cccb24ca2d734c5f7a3781594743e8772c3a
NutsNBoltz2882 <149544692+NutsNBoltz2882@users.noreply.github.com>
2024-03-24 11:15:32 -0500

    null auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -120,6 +120,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return new PathPlannerAuto("New Auto");
+    return null;
   }
 }
